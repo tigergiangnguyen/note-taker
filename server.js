1aaacb6a712ca55cea7830cc787e0413e33ec778
tigergiangnguyen <tigernguyen5@yahoo.com>
2023-04-11 22:51:33 -0400

    Use the precise routes for api and html

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
-
+const apiRoutes = require('./routes/api');
+const htmlRoutes = require('./routes/html');
 const PORT = process.env.PORT || 3001;
 
 const app = express();
@@ -7,8 +8,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
-
-app.listen(PORT, () =>
+app.listen(PORT, () => {
   console.log(`App listening at http://localhost:${PORT} 🚀`)
-);
+});
