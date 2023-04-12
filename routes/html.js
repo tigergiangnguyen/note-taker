94427835db0ddc850a27193dea053cb58a7286d4
tigergiangnguyen <tigernguyen5@yahoo.com>
2023-04-11 22:54:14 -0400

    Add a route that targets index.html

--- routes/html.js
+++ routes/html.js
@@ -1,12 +1,16 @@
 const router = require('express').Router();
 const path = require('path');
 
-router.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, 'index.html'))
-);
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'))
+});
 
-router.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, 'notes.html'))
-);
+router.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/notes.html'))
+});
+
+router.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'))
+});
 
 module.exports = router;
